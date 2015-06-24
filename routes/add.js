848a2db77d96efbe8b85fca65e35b35b38037729
Christian Ayscue <coayscue@gmail.com>
2015-06-24 13:01:23 -0400

    Post functionality:

---
+++ routes/add.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* get add post page  */
+router.get("/", function(req, res, next){
+  res.render("add_page", { title: "Add a page"});
+});
+
+router.post("/submit", function(req,res,next){
+  var models = require("../models/index.js");
+  var urlName;
+  if(typeof req.body.pageTitle != "undefined" && req.body.pageTitle !== ""){
+    urlName = req.body.pageTitle.toLowerCase().replace(/ /gi,"_");
+  }else{
+    urlName = Math.random().toString(36).substring(2,7);
+  }
+
+  var page = new models.Page({"title": req.body.pageTitle, "content":req.body.content, "url_name": urlName});
+  page.save();
+  //res.send(page);
+  res.redirect("/");
+});
+
+module.exports = router;
